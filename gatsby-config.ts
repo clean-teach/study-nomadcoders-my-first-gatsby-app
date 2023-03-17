c5ccb671109773afa1be50857892d5fa7d5f91b5
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-03-18 03:12:29 +0900

    #10.11 MDX 및 .prettierrc 추가
    ## 목표
    
    - `gatsby-source-filesystem` 이 내 컴퓨터에 있는 파일의 이름을 알아낼 수는 있지만, 파일의 내용에 접근하는 기능이 필요하다.
    
    ## ****`gatsby-plugin-mdx`****
    
    [gatsby-plugin-mdx | Gatsby](https://www.gatsbyjs.com/plugins/gatsby-plugin-mdx)
    
    - `gatsby-plugin-mdx`는 Gatsby와 함께 `MDX`를 사용하기 위한 공식 통합
    - `MDX`는 **마크다운** 안에 포함된 `JSX`를 작성할 수 있다.
        - 사소한 일에는 마크다운의 간결한 구문(예: # heading)을 사용하고, 고급 구성 요소에는 JSX를 사용할 수 있기 때문에 훌륭한 조합
    
    ### 사용방법
    
    1. 설치
    
        `npm install gatsby-plugin-mdx gatsby-source-filesystem @mdx-js/react`
    
    2. `gatsby-config.ts` 환경설정
    
        ```tsx
        const config: GatsbyConfig = {
          ...
          plugins: [
        		`gatsby-plugin-mdx`,
            {
              resolve: `gatsby-source-filesystem`,
              options: {
                path: `${__dirname}/blog-posts`,
              },
            },
          ],
        }
        ```
    
        - ``gatsby-source-filesystem`` 설정 내용(object) 위에 `**`gatsby-plugin-mdx`**` 를 적어 넣는다.
    3. 마크다운 파일의 확장자를 `.mdx` 로 수정
    4. `.mdx` 파일 작성 (예시)
    
        ```markdown
        ---
        title: Hello
        category: personal
        date: 2023-03-18
        auther: 니꼬
        ---
    
        ## Hello everyone!
    
        Welcome to my blog post. I'm very happy to have you all here with me on this spacial occasion.
        ```
    
        - `frontmatter` : 대쉬`-` 세개로 구분된 섹션 안에 있는 내용
        - **참고** :
            - Visual Studio Code 사용시 `MDX` 파일에서 `MDX` 문법에 따른 색상 표시 확장 프로그램
    
                → “`MDX`" 익스텐션 설치
    
    5. **`GraphQL` Explorer 접속**
    
        [http://localhost:8000/___graphql](http://localhost:8000/___graphql)
    
    6. query 설정하기
    
        ```graphql
        query MyQuery {
          allMdx {
            nodes {
              frontmatter {
                auther
                category
                date(formatString: "YYYY.MM.DD")
                title
              }
              excerpt(pruneLength: 25)
            }
          }
        }
        ```
    
        - `formatString` : 날짜 표기 방식 설정
        - `excerpt` : 마크다운 문법으로 작성된 내용 미리보기
            - `pruneLength` : 말줄임표로 끊을 최대 글자 수
    7. page 파일에서 **`GraphQL`** 데이터에 접근
    
        ```tsx
        export const query = graphql`
          query BlogPost {
            allMdx {
              nodes {
                frontmatter {
                  auther
                  category
                  date(formatString: "YYYY.MM.DD")
                  title
                }
                excerpt(pruneLength: 25)
              }
            }
          }
        `;
        ```
    
    8. page 컴포넌트에 props로 전달된 쿼리 사용
    
        ```tsx
        export default function Blog({ data }: PageProps<Queries.BlogPostQuery>) {
          console.log(data);
          return (
            <Layout title="Blog">
              <p>The most recent news from my shop</p>
              <section>
                {data.allMdx.nodes.map((file, index) => (
                  <article key={index}>
                    <h3>{file.frontmatter?.title}</h3>
                    <h5>
                      {file.frontmatter?.auther} in : {file.frontmatter?.category}
                    </h5>
                    <h6>{file.frontmatter?.date}</h6>
                    <hr />
                    <p>{file.excerpt}</p>
                  </article>
                ))}
              </section>
            </Layout>
          );
        }
        ```

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,16 +1,17 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'DevStickers',
     siteUrl: `https://www.yourdomain.tld`,
-    description: 'Buy the best stickers'
+    description: 'Buy the best stickers',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    `gatsby-plugin-mdx`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,6 +19,6 @@ const config: GatsbyConfig = {
       },
     },
   ],
-}
+};
 
-export default config
+export default config;
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,28 +1,44 @@
-import { graphql, PageProps } from "gatsby";
-import React from "react";
-import Layout from "../components/Layout";
-import Seo from "../components/Seo";
+import { graphql, PageProps } from 'gatsby';
+import React from 'react';
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
-export default function Blog({data}: PageProps<Queries.BlogTitlesQuery>) {
+export default function Blog({ data }: PageProps<Queries.BlogPostQuery>) {
   console.log(data);
-    return (
+  return (
     <Layout title="Blog">
       <p>The most recent news from my shop</p>
-      <ul>
-        {data.allFile.nodes.map((file, index) => <li key={index}>{file.name}</li>)}
-      </ul>
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article key={index}>
+            <h3>{file.frontmatter?.title}</h3>
+            <h5>
+              {file.frontmatter?.auther} in : {file.frontmatter?.category}
+            </h5>
+            <h6>{file.frontmatter?.date}</h6>
+            <hr />
+            <p>{file.excerpt}</p>
+          </article>
+        ))}
+      </section>
     </Layout>
-    );
+  );
 }
 
 export const query = graphql`
-  query BlogTitles {
-    allFile {
+  query BlogPost {
+    allMdx {
       nodes {
-        name
+        frontmatter {
+          auther
+          category
+          date(formatString: "YYYY.MM.DD")
+          title
+        }
+        excerpt(pruneLength: 30)
       }
     }
   }
 `;
 
-export const Head = () => <Seo title="Blog" />
+export const Head = () => <Seo title="Blog" />;
