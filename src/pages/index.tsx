0dc50179244766b1bff17d7a9b2087c039cc437d
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-03-22 00:59:55 +0900

    #10.14 StaticImage
    ## 목표
    
    - Gatsby 프로젝트에 이미지를 삽입해 본다.
    
    ## `gatsby-plugin-sharp`
    
    [gatsby-plugin-sharp | Gatsby](https://www.gatsbyjs.com/plugins/gatsby-plugin-sharp)
    
    ### 사용법
    
    1. 설치 :
    
        `npm install gatsby-plugin-sharp`
    
    ### [sharp](https://github.com/lovell/sharp)
    
    - 일반적인 형식의 큰 이미지를 더 작고 웹 친화적인 JPEG, PNG, WebP, GIF 및 다양한 크기의 AVIF 이미지로 변환하는 것
    
    ## `gatsby-plugin-image`
    
    [gatsby-plugin-image | Gatsby](https://www.gatsbyjs.com/plugins/gatsby-plugin-image)
    
    ### 사용법
    
    1. 설치 :
    
        `npm install gatsby-plugin-image gatsby-plugin-sharp gatsby-source-filesystem gatsby-transformer-sharp`
    
        - 해당 플러그인을 사용하기 위해서는 `[gatsby-plugin-sharp](https://www.notion.so/10-BONUS-GATSBY-101-549a88c286b44e05822abcafe76c4e08)` 또한 필요하므로, 한번에 설치 한다.
    2. `gatsby-config.ts` 파일 수정
    
        ```tsx
        module.exports = {
          plugins: [
            `gatsby-plugin-image`,
            `gatsby-plugin-sharp`,
            `gatsby-transformer-sharp`, // Needed for dynamic images
          ],
        }
        ```
    
        1. Gatsby 서버 재시작
    3. Static Image 사용하기
    
        ```tsx
        **import { StaticImage } from `gatsby-plugin-image`;**
    
        export default function ComponentName() {
        	return
        		**<StaticImage src='' />**
        }
        ```
    
        1. **`StaticImage` import**
        2. **`<StaticImage />`** 에 `src` 지정하기
            - 인터넷 브라우저에서 이미지를 찾아서 **“이미지 주소 복사”** 로 이미지의 주소를 `src` 값으로 지정
        - Static Image 란?
            - 정적 이미지
            - 미리 알 수 있다.
        - Dynamic Image 란?
            - 동적 이미지
            - 미리 알 수 없다.
    
    ### 결과
    
    - 자동으로 **반응형** 이미지가 적용된 HTML 코드와 이미지가 제공된다.
    - 만약, 수동으로 이미지의 사이즈를 설정한다면 자동으로 해당 사이즈로 **최적화** 된 이미지를 제공한다.
    
    → 사용자가 원본 그대로의 고사양 이미지를 다운 받지 않고, 상황에 따른 최적화된 이미지를 다운로드 받게 하여 결과적으로 더 나은 사용자 경험을 하게 한다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import * as React from "react"
-import Layout from "../components/Layout"
-import Seo from "../components/Seo"
-
+import { StaticImage } from 'gatsby-plugin-image';
+import * as React from 'react';
+import Layout from '../components/Layout';
+import Seo from '../components/Seo';
 
 export default function IndexPage() {
   return (
     <Layout title="Welcome to DevStickers">
       <p>Hello Welcome to Home</p>
+      <StaticImage
+        src="https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+        alt="개"
+      />
     </Layout>
-  )
+  );
 }
 
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Home" />;
