63622d8bfd08d88c93481729c4c66656a98c40ee
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-03-21 23:12:22 +0900

    #10.13 Blog Post
    ## 목표
    
    - URL 파라미터 템플렛 페이지에서 Props 를 받아본다.
    - 각각의 `MDX` 파일 내용 (`frontmatter`, markdown 문법)을 템플릿 페이지에서 HTML 로 렌더링 해 본다.
    
    ## Props
    
    ```tsx
    export default function Blog(**props**) {
      console.log(props);
      return (
    ...
    ```
    
    - Gatsby 프레임워크에서는 기본적으로 URL 파라미터 템플렛 페이지에 Props를 제공한다.
    - 이 props는 해당 URL 페이지에 대한 갖가지 정보를 담고 있다.
    
    ### `pageContext`
    
    - 해당 페이지의 `frontmatter` 과 id 정보 등을 가져올 수 있는 props의 key
    
    ## 구현
    
    1. query 에서 **slug**를 요청
    
        ```tsx
        export const query = graphql`
          query MyQuery(**$frontmatter__slug: String**) {
        	  mdx(frontmatter: {slug: {eq: $frontmatter__slug}}) {
        	    frontmatter {
        	      auther
        	      category
        	      date
        	      slug
        	    }
        	    body
        	  }
        	}
        `;
        ```
    
        - query 에서도 Props로 **`$frontmatter__slug`** 를 통하여 현재 URL 페이지의 파라미터에 접근 할 수 있다.
        - `mdx` → `frontmatter` → `slug` 에 일치하는 slug을 입력하여, 해당하는 `MDX` 파일에 접근 할 수 있다.
    2. props의 **data** key로 query 로 요청한 정보 받기
    
        ```tsx
        export default function Blog({**data**}) {
          console.log(**data**);
          return (
        ...
        ```
    
    3. [props가 제공](https://www.notion.so/10-BONUS-GATSBY-101-549a88c286b44e05822abcafe76c4e08)하는 **children** 으로 `MDX` 파일의 마크다운 부분도 불러 올 수 있다.
    
        ```tsx
        export default function Blog({**children**}) {
          console.log(children);
          return (
        		<div>{children}</div>
        ...
        ```
    
        → `MDX` 파일의 마크다운 문법을 HTML Element 로 변환해서 제공해준다.
    
    4. typescript 환경이라면, **interface**를 사용해서 props 의 type을 정의 해준다.
    
        ```tsx
        interface IProps {
        	data: QueryNameQuery;
        	children: any;
        }
    
        export default function Blog({data, children}: IProps) {
        ...
        ```
    
    5. 페이지의 Head Title에도 `MDX` 제목 적용
    
        ```tsx
        export const Head = ({data}) => <Seo title={data.mdx.frontmetter.title} />
        ```
    
        - Gatsby는 Head 컴포넌트에도 query 데이터를 전달 해준다.
    
    ## 참고
    
    - `[MDX`를 사용하도록 데이터 변환 →](https://www.gatsbyjs.com/docs/tutorial/part-5/)

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,10 +1,37 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../../components/Layout';
+import Seo from '../../components/Seo';
 
-export default function BlogPost() {
+interface IBlogPostProps {
+  data: Queries.PostDetailQuery;
+  children: any;
+}
+
+export default function BlogPost({ data, children }: IBlogPostProps) {
   return (
-    <Layout title="Blog Post">
-      <div></div>
+    <Layout title={data.mdx?.frontmatter?.title!}>
+      <div>{children}</div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query PostDetail($frontmatter__slug: String) {
+    mdx(frontmatter: { slug: { eq: $frontmatter__slug } }) {
+      frontmatter {
+        auther
+        category
+        date
+        slug
+        title
+      }
+      body
+    }
+  }
+`;
+
+export const Head = (props: any) => {
+  console.log(props);
+  return <Seo title={props.pageContext.frontmatter.title} />;
+};
