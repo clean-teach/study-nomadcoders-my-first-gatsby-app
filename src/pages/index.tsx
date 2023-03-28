febe253c422a0ab966dc721f1672e2d555829993
clean-teach <82921898+clean-teach@users.noreply.github.com>
2023-03-29 01:10:57 +0900

    #10.18 PicoCSS
    ## 목표
    
    - Gatsby 프로젝트의 Style을 꾸며보자
    
    ## 구현
    
    1. `src/styles.css` 파일 생성
    2. 프로젝트의 Root 디렉토리에 `gatsby-browser.ts` 파일 생성하여, CSS 파일 연결
    
        ```tsx
        import './src/styles.css';
        ```
    
        1. 프로젝트에 포함되었으면 하는 모든 것들을 작성
    
    ### [Gatsby Browser APIs](https://www.gatsbyjs.com/docs/reference/config-files/gatsby-browser/)
    
    - gatsby-browser.jsx/gatsby-browser.tsx 파일을 사용하면 브라우저 내에서 Gatsby 관련 이벤트에 응답하고 page 컴포넌트를 추가적으로 global 컴포넌트로 래핑할 수 있다.
    - Gatsby Browser API는 Gatsby의 클라이언트측과 상호 작용하기 위한 다양한 옵션을 제공
    
    1. `PicoCSS` 사용하여 프로젝트 꾸미기
    
    ### `PicoCSS`
    
    - 시맨틱 HTML을 위한 미니멀 CSS 프레임워크
    - CSS나 Class 없이 Styling 할 수 있다.
    
    [Pico.css • Minimal CSS Framework for semantic HTML](https://picocss.com/)
    
    - **[사용법** :](https://picocss.com/docs/)
        1. Install from CDN URL 복사
            - [https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css](https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css)
        2. `src/styles.css` 파일에서 해당 URL import
    
            ```css
            @import url('https://unpkg.com/@picocss/pico@1.5.7/css/pico.min.css');
            ```
    
            → 뿐만 아니라, 모든 요소에 기본적인 `Pico CSS`의 Style이 적용
    
            - 다크모드로 변경
    
                → 윈도우나 맥 내에서 다크모드로 설정하면, `Pico` 의 다크모드가 적용
    
        3. `Pico` Class 사용
            - `container`
    
                ```tsx
                ...
                <div className='container'></div>
                ```
    
                → 해당 요소가 페이지의 중앙에 정렬(text 중앙 정렬이 아닌, 레이아웃 중앙 정렬)된다.
    
            - `grid`
    
                ```tsx
                ...
                <section className='grid'>
                	<article></article>
                	<article></article>
                </section>
                ```
    
                → 자식요소가 grid 형식의 레이아웃으로 배치된다.
    
    ## 정리
    
    - Gatsby 프로젝트에 Style을 적용하는 방법을 배워 보았다.
    - `PicoCSS` 를 사용하면, 부트스트렙 처럼 쉽게 Style을 적용 할 수 있다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,18 +8,20 @@ export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
   return (
     <Layout title="Welcome to DevStickers">
       <p>Hello Welcome to Home</p>
-      {data.allContentfulStickerPack.nodes.map((sticker) => (
-        <article>
-          <GatsbyImage
-            image={getImage(sticker.preview?.gatsbyImageData!)!}
-            alt={sticker.name!}
-          />
-          <Link to={`/products/${sticker.id}`}>
-            <h2>{sticker.name}</h2>
-            <h3>{sticker.price}</h3>
-          </Link>
-        </article>
-      ))}
+      <div className="grid">
+        {data.allContentfulStickerPack.nodes.map((sticker) => (
+          <article key={sticker.id}>
+            <GatsbyImage
+              image={getImage(sticker.preview?.gatsbyImageData!)!}
+              alt={sticker.name!}
+            />
+            <Link to={`/products/${sticker.id}`}>
+              <h2>{sticker.name}</h2>
+              <h3>{sticker.price}</h3>
+            </Link>
+          </article>
+        ))}
+      </div>
       <StaticImage
         src="https://images.unsplash.com/photo-1596492784531-6e6eb5ea9993?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
         alt="개"
